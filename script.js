948aeb5a48a4e2ac4d706ec3f88e55a644f649e5
dul <eadndp@gmail.com>
2024-07-20 19:55:51 +0530

    crrnt plyer

--- script.js
+++ script.js
@@ -21,4 +21,5 @@ const winningCombos = [
 function startGame(){
     player1 = document.getElementById('player1').value || 'Player 1';
     player2 = document.getElementById('player2').value || 'Player 2';
+    currentPlayer = 'X';
 }
